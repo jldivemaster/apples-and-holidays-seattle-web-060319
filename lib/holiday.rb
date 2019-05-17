bd4e22e18aa1358df7d522c4fa92dddeca2f650c
Jason Leach <jlspursfan@gmail.com>
2019-05-17 22:33:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,5 +70,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |season|
+    season.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        array << holiday
+      end
+    end
+  end
+  return array
 end
